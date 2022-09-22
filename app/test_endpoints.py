22287b2a41b7f54e9cfdb5a2f3002897fc9826e8
sagoresarker <sagoresarker.cse@gmail.com>
2022-09-22 12:17:50 +0600

    Add pre-commit functionalities

---
+++ app/test_endpoints.py
@@ -0,0 +1,21 @@
+from urllib import response
+import pytest
+from fastapi.testclient import TestClient
+from app.main import app
+
+
+client = TestClient(app)
+
+
+def test_get_home():
+    response = client.get('/')
+    assert response.text != "<h1>hello</h1>"
+    assert response.status_code == 200
+    assert "text/html" in response.headers['content-type']
+
+
+def test_post_home():
+    response = client.post('/')
+    assert response.status_code == 200
+    assert "application/json" in response.headers['content-type']
+    assert response.json() == {'Hello': 'World'}
